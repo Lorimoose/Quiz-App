1f86e1836b3b8f8b54002a2b650b7d618b340752
Lori Moose <lorimoose@Loris-Air.lan>
2023-06-02 17:44:05 -0500

    added more questions, score, question x of y

--- script.js
+++ script.js
@@ -2,7 +2,10 @@ const startButton = document.getElementById('start-btn')
 const nextButton = document.getElementById('next-btn')
 const questionContainerElement = document.getElementById('question-container')
 const questionElement = document.getElementById('question')
+const questionNumber = document.getElementById('question-number')
 const answerButtonsElement = document.getElementById('answer-buttons')
+const scoreTracker = document.getElementById('score-tracker');
+const scoreUpElement = document.getElementById('score-up');
 
 let shuffledQuestions, currentQuestionIndex
 
@@ -17,7 +20,10 @@ function startGame() {
     shuffledQuestions = questions.sort(() => Math.random() - .5)
     currentQuestionIndex = 0
     questionContainerElement.classList.remove('hide')
+    questionNumber.classList.remove('hide')
     setNextQuestion()
+    scoreTracker.classList.remove('hide');
+    scoreUpElement.textContent = 0;
 }
 
 function setNextQuestion() {
@@ -50,6 +56,7 @@ function resetState() {
 function selectAnswer(e) {
     const selectedButton = e.target
     const correct = selectedButton.dataset.correct
+    processResults(correct);
     setStatusClass(document.body, correct)
     Array.from(answerButtonsElement.children).forEach(button => {
         setStatusClass(button, button.dataset.correct)
@@ -76,6 +83,16 @@ function clearStatusClass(element) {
     element.classList.remove('wrong')
 }
 
+function processResults(isCorrect) {
+    if (!isCorrect) {
+        return;
+    }
+
+    const scoreUp = parseInt(scoreUpElement.textContent, 10) || 0;
+
+    scoreUpElement.textContent = scoreUp + 10;
+}
+
 const questions = [
     {
         question: 'How many Infinity Stones are there?',
@@ -89,17 +106,17 @@ const questions = [
     {
         question: 'What type of scientist is Jane Foster in Thor?',
         answers: [
-            { text: 'Astronomer', correct: true },
             { text: 'Biologist', correct: false },
             { text: 'Chemist', correct: false },
+            { text: 'Astronomer', correct: true },
             { text: 'Dioptrics', correct: false },
         ]
     },
     {
         question: 'During which war did Captain America get his superhuman abilities?',
         answers: [
-            { text: 'World War II', correct: true },
             { text: 'Civil War', correct: false },
+            { text: 'World War II', correct: true },
             { text: 'Cold War', correct: false },
             { text: 'World War I', correct: false },
         ]
@@ -111,6 +128,60 @@ const questions = [
             { text: 'Spiderman', correct: false },
             { text: 'Doctor Strange', correct: false },
             { text: 'Black Panther', correct: false },
+        ]
+    },
+    {
+        question: 'Who is Loki\'s father?',
+        answers: [
+            { text: 'Odin', correct: false },
+            { text: 'Ronan the Accuser', correct: false },
+            { text: 'Thor', correct: false },
+            { text: 'Laufey', correct: true },
+        ]
+    },
+    {
+        question: 'Which fictional place is the setting of Black Panther?',
+        answers: [
+            { text: 'Maveth', correct: false },
+            { text: 'Wakanda', correct: true },
+            { text: 'Vormir', correct: false },
+            { text: 'Asgard', correct: false },
+        ]
+    },
+    {
+        question: 'How many years was Scott Lang trapped in the Quantum Realm?',
+        answers: [
+            { text: '10', correct: false },
+            { text: '3', correct: false },
+            { text: '6', correct: false },
+            { text: '5', correct: true },
+        ]
+    },
+    {
+        question: 'Which material composes Captain America\'s shield?',
+        answers: [
+            { text: 'Vibranium', correct: true },
+            { text: 'Uru', correct: false },
+            { text: 'Carbonadium', correct: false },
+            { text: 'Iron', correct: false },
+        ]
+    },
+    {
+        question: 'What is the name of Peter Parker\'s best friend?',
+        answers: [
+            { text: 'Mary Jane', correct: false },
+            { text: 'Sean', correct: false },
+            { text: 'Ned', correct: true },
+            { text: 'Carl', correct: false },
+        ]
+    },
+    {
+        question: 'How many children does Hawkeye have?',
+        answers: [
+            { text: '2', correct: false },
+            { text: '1', correct: false },
+            { text: '3', correct: true },
+            { text: '0', correct: false },
         ]
     },
 ]
